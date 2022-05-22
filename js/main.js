39279458e8e076a780fba6c1d482a3c2dd4f7bbb
ChefDimas <dimalunik@aol.com>
2022-05-23 01:41:10 +0300

    Changing mrkup

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 $(function () {
   $(".bike__slider").slick({
       arrows: false,
-      dots: true
+      dots: true,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 2000
   });
 });
